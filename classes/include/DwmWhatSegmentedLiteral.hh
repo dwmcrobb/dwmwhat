1ffad7d0d6cf0339a2d0290878f8be0f9caa20da
dwm <dwm@mcplex.net>
2025-11-14 21:52:49 -0500

    Refactor so I don't need deuciotn guide.

--- classes/include/DwmWhatSegmentedLiteral.hh
+++ classes/include/DwmWhatSegmentedLiteral.hh
@@ -42,7 +42,6 @@
 #ifndef _DWMWHATSEGMENTEDLITERAL_HH_
 #define _DWMWHATSEGMENTEDLITERAL_HH_
 
-
 #include <algorithm>
 #include <cassert>
 #include <cstdint>
@@ -57,65 +56,56 @@ namespace Dwm {
   namespace What {
 
     //------------------------------------------------------------------------
-    //!  The idea of this template: hold a concatenated string literal that
-    //!  is constructed from a variadic list of string literals, with the
-    //!  length of each string literal stored in an array.  This allows one
-    //!  string literal to be stored in a binary, but each 'segement' is
-    //!  retrievable via the @c nth() member.
-    //!  This is primarily here to support the Dwm::What::Info class, but
-    //!  I split it out simply because it's useful for other things.
-    //!  You can do this:
-    //!
-    //!  SegmentedLiteral foo("foo","1.0.1","My Name 2025",__DATE__,__FILE__);
-    //!
-    //!  and be able to pull out "foo" with nth(0), "1.0.1" with nth(1),
-    //!  etc.
-    //!
-    //!  Probably worth noting some of the goals of this class template:
-    //!   - Keep a contiguous single string literal so it can be found as
-    //!     one string in a binary.
-    //!   - Try to minimize the space utilized for the array of segment
-    //!     lengths.
-    //!   - Almost everything needs to be done at compile time so that
-    //!     the compiler is aware of a single string literal ("constexpr
-    //!     all the things", Jason Turner).
+    //!  
     //------------------------------------------------------------------------
-    template <std::size_t DelimLen, std::size_t NumSegs, std::size_t NumChars>
+    template <std::size_t DelimLen, std::size_t FirstLen, std::size_t ...N>
     class SegmentedLiteral
     {
     public:
+      template <std::size_t D, std::size_t ...Size>
+      struct SegmentedLiteralChars {
+        static const size_t sz =
+          ((D - 1) * (sizeof...(Size) - 1)) + ((Size-1) + ...) + 1;
+      };
+
+      static constexpr size_t SegmentedLiteralChars_v =
+        SegmentedLiteralChars<DelimLen,FirstLen,N...>::sz;
+
+      static constexpr size_t NumSegs_v = sizeof...(N) + 1;
+
       using SegLenType =
-        std::conditional<(NumChars <= 256),
+        std::conditional<(SegmentedLiteralChars_v <= 256),
                          uint8_t,
-                         typename std::conditional<(NumChars <= 65536),
+                         typename std::conditional<(SegmentedLiteralChars_v <= 65536),
                                                    uint16_t,
                                                    uint32_t>::type>::type;
-      using BufType = const char(&)[NumChars];
+      using BufType = const char(&)[SegmentedLiteralChars_v];
 
       //----------------------------------------------------------------------
       //!  
       //----------------------------------------------------------------------
-      template <std::size_t D, std::size_t F, std::size_t ...Ns>
-      consteval SegmentedLiteral(const char (&delim)[D], const char (&f)[F],
-                                 const char (&...s)[Ns])
+      consteval SegmentedLiteral(const char (&delim)[DelimLen],
+                                 const char (&f)[FirstLen],
+                                 const char (&...s)[N])
       {
-        static_assert((NumChars) <= std::numeric_limits<SegLenType>::max());
+        static_assert(SegmentedLiteralChars_v
+                      <= std::numeric_limits<SegLenType>::max());
 
         //  'f' is just 'first'
-        auto  it = std::ranges::copy_n(f,F-1,_buffer).out;
+        auto  it = std::ranges::copy_n(f,FirstLen-1,_buffer).out;
         std::size_t  si = 0;
-        seglengths[si++] = F-1;
-        ((seglengths[si++] = Ns-1,
-          it = std::ranges::copy_n(delim,D-1,it).out,
-          it = std::ranges::copy_n(s,Ns-1,it).out), ...);
+        seglengths[si++] = FirstLen-1;
+        ((seglengths[si++] = N-1,
+          it = std::ranges::copy_n(delim,DelimLen-1,it).out,
+          it = std::ranges::copy_n(s,N-1,it).out), ...);
         *it = '\0';
       }
-      
+
       //----------------------------------------------------------------------
       //!  Returns a view of the whole buffer, minus the terminating null.
       //----------------------------------------------------------------------
       consteval operator std::string_view () const noexcept
-      { return std::string_view(_buffer,size-1); }
+      { return std::string_view(_buffer,SegmentedLiteralChars_v-1); }
 
       //----------------------------------------------------------------------
       //!  Returns the buffer.
@@ -127,22 +117,22 @@ namespace Dwm {
       //!  Returns a view of the whole buffer, minus the terminating null.
       //----------------------------------------------------------------------
       constexpr std::string_view view() const noexcept
-      { return std::string_view(_buffer,size-1); }
+      { return std::string_view(_buffer,SegmentedLiteralChars_v-1); }
         
       //----------------------------------------------------------------------
       //!  Returns the number of segments in the buffer (1 or more).
       //----------------------------------------------------------------------
       constexpr std::size_t num_segments() const noexcept
-      { return NumSegs; }
+      { return NumSegs_v; }
       
       //----------------------------------------------------------------------
       //!  Returns a view of the nth segment.
       //----------------------------------------------------------------------
       constexpr std::string_view nth(std::size_t n) const noexcept
       {
-        assert(n < NumSegs);
+        assert(n < NumSegs_v);
         std::size_t  off = std::accumulate(seglengths, &seglengths[n], 0);
-        off += (n - 0) * delimLen;
+        off += n * (delimLen - 1);
         return std::string_view(_buffer + off, seglengths[n]);
       }
       
@@ -151,44 +141,12 @@ namespace Dwm {
       //----------------------------------------------------------------------
       constexpr std::size_t size_of_seg_lengths() const noexcept
       { return sizeof(SegLenType); }
-
-    protected:
-      char         _buffer[NumChars] {};
-      std::size_t  size = NumChars;
-      SegLenType   seglengths[NumSegs] {};
+      
+    private:
+      char         _buffer[SegmentedLiteralChars_v] {};
+      SegLenType   seglengths[NumSegs_v] {};
       std::size_t  delimLen = DelimLen;
     };
-
-    //------------------------------------------------------------------------
-    //!  Just a helper to hold the number of characters we need for the
-    //!  buffer based on the template arguments.  We need space for N
-    //!  segments and N-1 delimiters.
-    //------------------------------------------------------------------------
-    template <std::size_t D, std::size_t ...Ns>
-    struct SegmentedLiteralChars {
-      static const size_t sz =
-        ((D - 1) * (sizeof...(Ns) - 1)) + ((Ns-1) + ...) + 1;
-    };
-
-    template <std::size_t D, std::size_t ...Ns>
-    constexpr size_t SegmentedLiteralChars_v =
-      SegmentedLiteralChars<D,Ns...>::sz;
-    
-    //------------------------------------------------------------------------
-    //!  Deduction guide.  This is critical, since we need to deduce DelimLen,
-    //!  NumSegs and NumChars to instantiate the template.  We deduce
-    //!  DelimLen from the length of delim, minus 1 (remove null).  We
-    //!  deduce the number of segments from the number of variadic
-    //!  arguments (sizeof...(Ns)).  To deduce NumChars, we add up the
-    //!  length of all of the variadic arguments (minus 1 for each to ignore
-    //!  null termination).  We then add the number of characters we need to
-    //!  store delimiter strings.  Here we need D-1 characters for each,
-    //!  and we need (sizeof...(Ns) - 1) of them (they only go between
-    //!  segments, not before the first segment or after the last segment).
-    //------------------------------------------------------------------------
-    template <std::size_t D, std::size_t ...Ns>
-    SegmentedLiteral(const char (&delim)[D], const char (&...s)[Ns])
-      -> SegmentedLiteral<D-1,sizeof...(Ns),SegmentedLiteralChars_v<D,Ns...>>;
     
   }  // namespace What
 
