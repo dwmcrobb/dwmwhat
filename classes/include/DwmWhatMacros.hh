32583cfcdf5003b96e82f17ad306d43242d0d86d
dwm <dwm@mcplex.net>
2025-11-29 14:25:56 -0500

    Tweak comment.

--- classes/include/DwmWhatMacros.hh
+++ classes/include/DwmWhatMacros.hh
@@ -110,7 +110,8 @@
 //----------------------------------------------------------------------------
 //!  If all you need is a version string and you're using C instead of C++,
 //!  the following macro can be used to create a string of the same format
-//!  as used by Dwm::What::Info, for example:
+//!  as used by Dwm::What::Info.  The parameters are the same as those of
+//!  the Dwm::What::Info constructor.  For example:
 //!
 //!  const char  my_prog_version[] =
 //!     DWM_WHAT_INFO("🤖","✅","myprog","1.0.0","© My Name 2025","my prog");
