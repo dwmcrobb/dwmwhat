fc6756b3faacd2016cb205ceddd3834d92b87d7e
dwm <dwm@mcplex.net>
2025-12-02 14:37:19 -0500

    Added DWM_WHAT_SYM_LEFT_MAGNIFY

--- classes/include/DwmWhatMacros.hh
+++ classes/include/DwmWhatMacros.hh
@@ -66,6 +66,7 @@
 #define DWM_WHAT_SYM_HEAVY_EXCL        "\xE2\x9D\x97"                   // ❗
 #define DWM_WHAT_SYM_HEAVY_PLUS        "\xE2\x9E\x95"                   // ➕
 #define DWM_WHAT_SYM_JACKOLANTERN      "\xF0\x9F\x8E\x83"               // 🎃
+#define DWM_WHAT_SYM_LEFT_MAGNIFY      "\xF0\x9F\x94\x8D"               // 🔍
 #define DWM_WHAT_SYM_OPEN_BOOK         "\xF0\x9F\x93\x96"               // 📖
 #define DWM_WHAT_SYM_OPEN_FOLDER       "\xF0\x9F\x93\x82"               // 📂
 #define DWM_WHAT_SYM_PACKAGE           "\xF0\x9F\x93\xA6"               // 📦
